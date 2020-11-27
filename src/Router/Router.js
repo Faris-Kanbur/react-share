ea49425646c7dd344551bb694a8e47e4bf7042f3
faris <faris38kanbur@gmail.com>
2020-11-27 20:33:15 +0100

    added Roter doc

---
+++ src/Router/Router.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import Signup from './page/Signup';
+
+function Router() {
+  return (
+    <>
+      <Navbar />
+      <Signup />
+      <Signup />
+     
+    </>
+  );
+}
+
+export default Router;
