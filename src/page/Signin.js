60748be2db188b13d053871891238f986bd2a6c6
Faris Kanbur <60715227+Faris-Kanbur@users.noreply.github.com>
2020-12-01 17:59:13 +0100

    Update Signin.js

--- src/page/Signin.js
+++ src/page/Signin.js
@@ -1,80 +1,85 @@
-import React from 'react'
-import { Button, TextField, Grid, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { Button, TextField, Grid, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import firebase from "../firebase/firebase.utils";
-import {Formik} from "formik"
-
+import { Formik } from "formik";
 
 const stylesFunc = makeStyles({
-    wrapper: {
-        marginTop: "10rem",
-    }
-})
+  wrapper: {
+    marginTop: "10rem",
+  },
+});
 
 const initialValues = {
-      email: '',
-      password: '',
-    }
-
+  email: "",
+  password: "",
+};
 
 function Signin() {
- 
-    const signupStyles = stylesFunc();
+  const signinStyles = stylesFunc();
 
-    const handleGoogleButtonClick = () =>{
-        firebase.useGoogleProvider();
-    } 
+  const handleGoogleButtonClick = () => {
+    firebase.useGoogleProvider();
+  };
 
-    const handleFormSubmit = (values) => {
-        firebase.signIn(values.email, values.password)
-    }
+  const handleFormSubmit = (values) => {
+    // alert(JSON.stringify(values, null, 2));
+    firebase.signIn(values.email, values.password);
+  };
 
-
-    return (
-        <Container className={signupStyles.wrapper} maxWidth="sm">
-            <Formik initialValues={initialValues} onSubmit = {handleFormSubmit}>
-                {({handleSubmit,handleChange, values}) => (
-                    <form  onSubmit={handleSubmit}>
-                        <Grid container spacing={3}>       
-                            <Grid item xs={12}>       
-                                <TextField 
-                                    name="email" 
-                                    label="Email" 
-                                    variant="outlined"  
-                                    fullWidth
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    />
-                            </Grid>
-                            <Grid item xs={12}>       
-                                <TextField 
-                                    name="password" 
-                                    label="Password" 
-                                    variant="outlined"
-                                    type="password"  
-                                    fullWidth
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    />
-                            </Grid>
-
-                            <Grid item xs={12}>   
-                                <Button type="submit" color="primary" variant="contained" fullWidth>Register</Button>
-                            </Grid>
-                            <Grid item xs={12}>   
-                                <Button 
-                                    onClick={handleGoogleButtonClick}
-                                    color="primary" 
-                                    variant="contained" 
-                                    fullWidth>Signup with Google
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </form> 
-                 )}  
-            </Formik>
-        </Container>
-    )
+  return (
+    <Container className={signinStyles.wrapper} maxWidth="sm">
+      <Formik initialValues={initialValues} onSubmit={handleFormSubmit}>
+        {({ handleSubmit, handleChange, values }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={values.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  name="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  fullWidth
+                  value={values.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={handleGoogleButtonClick}
+                >
+                  Sign In with Google
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
 
 export default Signin;
