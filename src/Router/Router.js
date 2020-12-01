a032b30bd73ed7751ef3053c1a8ed132ad2a87ce
Faris Kanbur <60715227+Faris-Kanbur@users.noreply.github.com>
2020-12-01 17:47:27 +0100

    reviewed again

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,19 +1,39 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { useContext } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Signup from '../page/Signup';
-import Signin from '../page/Signin';
-import Navbar from '../components/Navbar'
-
+import Main from "../pages/Main";
+import Signup from "../pages/Signup";
+import Signin from "../pages/Signin";
+import ForgotPassword from "../pages/ForgotPassword";
+import UserDetail from "../pages/UserDetail";
+import UserPost from "../pages/UserPost";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { FirebaseAuthContext } from "../context/AuthContext";
 
 function AppRouter() {
+  const { currentUser } = useContext(FirebaseAuthContext);
+
   return (
     <Router>
       <Navbar />
       <Switch>
-          <Signin path ="/register" components={Signup} />
-          <Signup path ="/login" components={Signin} />
+        <Route exact path="/register" component={Signup} />
+        <Route exact path="/login" component={Signin} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route
+          exact
+          path="/user/:id"
+          component={currentUser ? UserDetail : Signin}
+        />
+        <Route
+          exact
+          path="/user/:id/post"
+          component={currentUser ? UserPost : Signin}
+        />
+        <Route path="/" component={Main} />
       </Switch>
-     
+      <Footer />
     </Router>
   );
 }
