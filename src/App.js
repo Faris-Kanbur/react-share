04ad984e1cb6651ccdb4e15cbcb8d3c6e224593f
Faris Kanbur <60715227+Faris-Kanbur@users.noreply.github.com>
2020-12-01 17:46:18 +0100

    reviewed again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import AppRouter from './Router/Router';
-import AuthContextProvider from './context/AuthContext'
-
+import AppRouter from "./Router/Router";
+import AuthContextProvider from "./context/AuthContext";
 function App() {
   return (
-    < AuthContextProvider>
-        <AppRouter />
-    </ AuthContextProvider>
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   );
 }
 
 export default App;
+
